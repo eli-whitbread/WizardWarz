65c1258f8d22b1a06b6c63923d82c7359d60146b
Eli Whitbread <eli@whitbreadgames.com>
2016-06-22 20:40:06 +1000

    Added simple Grid building
    Added InitializeGameBoard() to MainWindow.xaml.cs - Initialises a simple
    game grid (using placeholder images) with only floor and outer walls.
    GameBoardManager is a WIP - trying to create a new Class for game grid
    building... not yet working!

--- GameBoardManager.cs
+++ GameBoardManager.cs
@@ -1,38 +1,29 @@
 ﻿using System;
+using System.Windows;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
-using Microsoft.Surface;
-using Microsoft.Surface.Core;
-using Microsoft.Surface.Presentation.Controls;
-using Microsoft.Surface.Presentation.Input;
+
 
 namespace WizardWarz
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Not yet working - can't draw grid to window. Refer MainWindow.xaml.cs "InitializeGameBoard()"
     /// </summary>
-    public partial class MainWindow : SurfaceWindow
+    public partial class GameBoardManager : MainWindow
     {
-        public Int32 tileSize = 64;
+        //public Int32 tileSize = 64;
 
-        public MainWindow()
-        {
-            InitializeComponent();
-
-            InitializeGameBoard();
-           
-        }
+        //public GameBoardManager()
+        //{
+        //    InitializeComponent();
+        //    InitializeGameBoard();
+        //}
 
         private void InitializeGameBoard()
         {
@@ -43,7 +34,6 @@ namespace WizardWarz
             GridLengthConverter myGridLengthConverter = new GridLengthConverter();
             GridLength side = (GridLength)myGridLengthConverter.ConvertFromString("Auto");
 
-            //Setup the grid Rows and Columns
             for (int i = 0; i < cols; i++)
             {
                 GameBoardGrid.ColumnDefinitions.Add(new System.Windows.Controls.ColumnDefinition());
@@ -55,29 +45,14 @@ namespace WizardWarz
                 GameBoardGrid.RowDefinitions[x].Height = side;
             }
 
-            //create an empty Rectangle array
             Rectangle[,] flrTiles = new Rectangle[cols, rows];
 
-            //fill each element in the Rectangle array with a FloorTile image.
             for (int c = 0; c < cols; c++)
             {
                 for (int r = 0; r < rows; r++)
                 {
-                    
                     flrTiles[c, r] = new Rectangle();
-                    //add a wall tile if along the grid extremes
-                    if (r == 0 || r == rows -1 || c == 0 || c == cols - 1)
-                    {
-                        flrTiles[c, r].Fill = new ImageBrush(new BitmapImage(new Uri(@".\Resources\WallTile_64x64.png", UriKind.Relative)));
-                    }
-                    //otherwise add a floor tile
-                    else
-                    {
-                        flrTiles[c, r].Fill = new ImageBrush(new BitmapImage(new Uri(@".\Resources\FloorTile_64x64.png", UriKind.Relative)));
-                    }
-                    //
-                    //inner solid and destrutable walls still required!!
-                    //
+                    flrTiles[c, r].Fill = new ImageBrush(new BitmapImage(new Uri(@"I:\nsquared Visual Studio project\WizardWarz\Resources\FloorTile_64x64.png", UriKind.Relative)));
                     flrTiles[c, r].Height = tileSize;
                     flrTiles[c, r].Width = tileSize;
                     Grid.SetColumn(flrTiles[c, r], c);
@@ -87,6 +62,7 @@ namespace WizardWarz
                 }
             }
         }
+
 
     }
 }
