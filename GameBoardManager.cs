54895454eff16fbaf1a653063c5e5d25b912e0b3
Eli Whitbread <eli@whitbreadgames.com>
2016-06-23 16:50:45 +1000

    Moved the GameGrid building to a new Class - "GameBoardManager"

--- GameBoardManager.cs
+++ GameBoardManager.cs
@@ -17,50 +17,63 @@ namespace WizardWarz
     /// </summary>
     public partial class GameBoardManager : MainWindow
     {
-        //public Int32 tileSize = 64;
-
-        //public GameBoardManager()
-        //{
-        //    InitializeComponent();
-        //    InitializeGameBoard();
-        //}
-
-        private void InitializeGameBoard()
+        
+        public static void InitializeGameBoard2(Grid gameGrid)
         {
+            Int32 tileSize = 64;
             //set the grid size
             Int32 rows = 12;
             Int32 cols = 12;
 
+            //setup an array of grid positions (int[,]) for inner walls
+            Int16[,] innerWallPos = { { 3, 5 }, { 5, 5 }  };
+
             GridLengthConverter myGridLengthConverter = new GridLengthConverter();
             GridLength side = (GridLength)myGridLengthConverter.ConvertFromString("Auto");
 
+            //Setup the grid Rows and Columns
             for (int i = 0; i < cols; i++)
             {
-                GameBoardGrid.ColumnDefinitions.Add(new System.Windows.Controls.ColumnDefinition());
-                GameBoardGrid.ColumnDefinitions[i].Width = side;
+                gameGrid.ColumnDefinitions.Add(new System.Windows.Controls.ColumnDefinition());
+                gameGrid.ColumnDefinitions[i].Width = side;
             }
             for (int x = 0; x < rows; x++)
             {
-                GameBoardGrid.RowDefinitions.Add(new System.Windows.Controls.RowDefinition());
-                GameBoardGrid.RowDefinitions[x].Height = side;
+                gameGrid.RowDefinitions.Add(new System.Windows.Controls.RowDefinition());
+                gameGrid.RowDefinitions[x].Height = side;
             }
 
+            //create an empty Rectangle array
             Rectangle[,] flrTiles = new Rectangle[cols, rows];
 
+            //fill each element in the Rectangle array with an image "tile".
             for (int c = 0; c < cols; c++)
             {
                 for (int r = 0; r < rows; r++)
                 {
                     flrTiles[c, r] = new Rectangle();
-                    flrTiles[c, r].Fill = new ImageBrush(new BitmapImage(new Uri(@"I:\nsquared Visual Studio project\WizardWarz\Resources\FloorTile_64x64.png", UriKind.Relative)));
+                    //add a wall tile if along the grid extremes
+                    if (r == 0 || r == rows - 1 || c == 0 || c == cols - 1)
+                    {
+                        flrTiles[c, r].Fill = new ImageBrush(new BitmapImage(new Uri(@".\Resources\WallTile_64x64.png", UriKind.Relative)));
+                    }
+                    //otherwise add a floor tile
+                    else
+                    {
+                        flrTiles[c, r].Fill = new ImageBrush(new BitmapImage(new Uri(@".\Resources\FloorTile_64x64.png", UriKind.Relative)));
+                    }
+                    //
+                    //inner solid and destrutable walls still required!!
+                    //
                     flrTiles[c, r].Height = tileSize;
                     flrTiles[c, r].Width = tileSize;
                     Grid.SetColumn(flrTiles[c, r], c);
                     Grid.SetRow(flrTiles[c, r], r);
 
-                    GameBoardGrid.Children.Add(flrTiles[c, r]);
+                    gameGrid.Children.Add(flrTiles[c, r]);
                 }
             }
+            
         }
 
 
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -30,63 +30,11 @@ namespace WizardWarz
         {
             InitializeComponent();
 
-            InitializeGameBoard();
+            GameBoardManager.InitializeGameBoard2(GameBoardGrid);
            
         }
 
-        private void InitializeGameBoard()
-        {
-            //set the grid size
-            Int32 rows = 12;
-            Int32 cols = 12;
-
-            GridLengthConverter myGridLengthConverter = new GridLengthConverter();
-            GridLength side = (GridLength)myGridLengthConverter.ConvertFromString("Auto");
-
-            //Setup the grid Rows and Columns
-            for (int i = 0; i < cols; i++)
-            {
-                GameBoardGrid.ColumnDefinitions.Add(new System.Windows.Controls.ColumnDefinition());
-                GameBoardGrid.ColumnDefinitions[i].Width = side;
-            }
-            for (int x = 0; x < rows; x++)
-            {
-                GameBoardGrid.RowDefinitions.Add(new System.Windows.Controls.RowDefinition());
-                GameBoardGrid.RowDefinitions[x].Height = side;
-            }
-
-            //create an empty Rectangle array
-            Rectangle[,] flrTiles = new Rectangle[cols, rows];
-
-            //fill each element in the Rectangle array with a FloorTile image.
-            for (int c = 0; c < cols; c++)
-            {
-                for (int r = 0; r < rows; r++)
-                {
-                    
-                    flrTiles[c, r] = new Rectangle();
-                    //add a wall tile if along the grid extremes
-                    if (r == 0 || r == rows -1 || c == 0 || c == cols - 1)
-                    {
-                        flrTiles[c, r].Fill = new ImageBrush(new BitmapImage(new Uri(@".\Resources\WallTile_64x64.png", UriKind.Relative)));
-                    }
-                    //otherwise add a floor tile
-                    else
-                    {
-                        flrTiles[c, r].Fill = new ImageBrush(new BitmapImage(new Uri(@".\Resources\FloorTile_64x64.png", UriKind.Relative)));
-                    }
-                    //
-                    //inner solid and destrutable walls still required!!
-                    //
-                    flrTiles[c, r].Height = tileSize;
-                    flrTiles[c, r].Width = tileSize;
-                    Grid.SetColumn(flrTiles[c, r], c);
-                    Grid.SetRow(flrTiles[c, r], r);
-
-                    GameBoardGrid.Children.Add(flrTiles[c, r]);
-                }
-            }
-        }
+        
 
     }
 }
