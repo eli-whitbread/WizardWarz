0707ebea15cf625e5a24733000c3f858e170af67
Hugh Elleman <SpartanH97@gmail.com>
2016-06-27 04:15:38 +1000

    Small edits to exisitng classes, no changes to functionality. Game Timer established, but I just can't wrap my head around integrating it properly.

---
+++ GameTimer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Windows;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Threading;
+using System.Timers;
+using System.Windows.Data;
+using Microsoft.Surface;
+using Microsoft.Surface.Core;
+using Microsoft.Surface.Presentation.Controls;
+using Microsoft.Surface.Presentation.Input;
+using System.Windows.Input;
+
+
+namespace WizardWarz
+{
+    public class GameTimer
+    {
+        public void loopTimer()
+        {
+            DispatcherTimer gameLoopTimer = new DispatcherTimer(DispatcherPriority.Render);
+            gameLoopTimer.Interval = TimeSpan.FromMilliseconds(FramesPerSecond());
+            gameLoopTimer.Tick += timer_Tick;
+            gameLoopTimer.Start();
+        }
+
+        public void timer_Tick(object sender, EventArgs e)
+        {
+            
+        }
+
+        double FramesPerSecond()
+        {
+            Int32 fps = 30;
+            Double frameTimeSpan;
+
+            frameTimeSpan = fps / 1000;
+
+            return frameTimeSpan;
+        }
+    }
+}
--- PlayerController.cs
+++ PlayerController.cs
@@ -8,6 +8,8 @@ using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Windows.Input;
+using WizardWarz;
 
 namespace WizardWarz
 {
@@ -29,8 +31,7 @@ namespace WizardWarz
             Grid.SetColumn(playerTile, 1);
             Grid.SetRow(playerTile, 1);
 
-            gameGrid.Children.Add(playerTile);
+            gameGrid.Children.Add(playerTile);          
         }
-
     }
 }
