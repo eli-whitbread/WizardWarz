d4cb283ac847b1759f19fe47bac28c696ba193ea
Eli Whitbread <eli@whitbreadgames.com>
2016-07-26 10:39:24 +1000

    Partial work on AnimatedTile... still not working!
    Just pushing a WIP animated tile Class, it isn't tied into any other
    class yet so yo can just ignore it. added a test spritesheet called
    ZombieHunter_SpriteSheet for testing (to be removed as soon as we have a
    replacement).

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -129,5 +129,15 @@ namespace WizardWarz.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap ZombieHunter_SpriteSheet {
+            get {
+                object obj = ResourceManager.GetObject("ZombieHunter_SpriteSheet", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
